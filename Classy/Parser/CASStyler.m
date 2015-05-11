cf26c4d0f6338b33d59713443dddad5b10170021
Sudeep Sidhu <s.sidhu@tippingcanoe.com>
2015-05-11 18:32:54 -0500

    Adding synchronized access to the scheduled items Adding in fix to do weak associations without crashing

--- Classy/Parser/CASStyler.m
+++ Classy/Parser/CASStyler.m
@@ -709,32 +709,44 @@ NSArray *ClassGetSubclasses(Class parentClass) {
 #pragma mark - sceduling
 
 - (void)updateScheduledItems {
-    for (id<CASStyleableItem> item in self.scheduledItems.allObjects.copy) {
-        if (!item) continue;
-        [item cas_updateStylingIfNeeded];
-    }
 
-    if (self.scheduledItems.allObjects.count == 0) {
-        [self.updateTimer invalidate];
-        self.updateTimer = nil;
+    @synchronized(self.scheduledItems) {
+        
+        for (id<CASStyleableItem> item in self.scheduledItems.allObjects.copy) {
+            if (!item) continue;
+            [item cas_updateStylingIfNeeded];
+        }
+
+        if (self.scheduledItems.allObjects.count == 0) {
+            [self.updateTimer invalidate];
+            self.updateTimer = nil;
+        }
     }
 }
 
 - (void)scheduleUpdateForItem:(id<CASStyleableItem>)item {
-    [self.scheduledItems addObject:item];
+    
+    @synchronized(self.scheduledItems) {
 
-    if (self.scheduledItems.allObjects.count && !self.updateTimer.isValid) {
-        self.updateTimer = [NSTimer timerWithTimeInterval:0.0 target:self selector:@selector(updateScheduledItems) userInfo:nil repeats:YES];
-        [NSRunLoop.mainRunLoop addTimer:self.updateTimer forMode:NSRunLoopCommonModes];
+        [self.scheduledItems addObject:item];
+
+        if (self.scheduledItems.allObjects.count && !self.updateTimer.isValid) {
+            self.updateTimer = [NSTimer timerWithTimeInterval:0.0 target:self selector:@selector(updateScheduledItems) userInfo:nil repeats:YES];
+            [NSRunLoop.mainRunLoop addTimer:self.updateTimer forMode:NSRunLoopCommonModes];
+        }
     }
 }
 
 - (void)unscheduleUpdateForItem:(id<CASStyleableItem>)item {
-    [self.scheduledItems removeObject:item];
+    
+    @synchronized(self.scheduledItems) {
+    
+        [self.scheduledItems removeObject:item];
 
-    if (self.scheduledItems.allObjects.count == 0) {
-        [self.updateTimer invalidate];
-        self.updateTimer = nil;
+        if (self.scheduledItems.allObjects.count == 0) {
+            [self.updateTimer invalidate];
+            self.updateTimer = nil;
+        }
     }
 }
 
